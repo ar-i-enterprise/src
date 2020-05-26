5c09aae75b38f44229d670f3f89804dc461398a3
AR-I Enterprise <55053935+ar-i-enterprise@users.noreply.github.com>
2020-05-26 22:46:25 +0200

    sdwd

--- gazebo_simulation_scene/scripts/joy_to_twist.py
+++ gazebo_simulation_scene/scripts/joy_to_twist.py
@@ -12,152 +12,57 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-import copy
 import rospy
-import numpy as np
-import moveit_commander
-import geometry_msgs.msg
+import numpy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+	
+ 
+def JoyToTwist():
+	# initialize node
+	rospy.init_node('JoyToTwist', anonymous=True)
 
+	# setup joy topic subscription
+	joy_subscriber = rospy.Subscriber("joy", Joy, handleJoyMsg, queue_size=10)
 
-# def move_ur5(msg, data):  # data is type of geometry_msgs.msg.Pose
-#     print('=======================')
-#     info = msg
-#     print(info)
-#     print('========= DATA ==============')
-#     print(data[1])
+	# spin() simply keeps python from exiting until this node is stopped
+	rospy.spin()
 
+	
+def handleJoyMsg(data):
+	#### Initialize Speed Parameter
+	#   axes  [l.x   l.y   l.z   a.x     a.y    a.z]
+	scalers = [0.7,  0.7,  0.7,  -3.14, -3.14, -3.14]	
+	
+	
+	#### Setup Twist Publisher 
+   	twist_publisher = rospy.Publisher("cmd_vel_mux/input/navi", Twist)
+	msg = Twist()
 
-# Handle ur5 movements
-def move_ur5(msg, args):
+	#### Start Mapping from Joy to Twist
+	if len(data.axes) >= 6 :
+		msg.angular.x = data.axes[5] * scalers[3]		
 
-    robot = args[0]
-    ur5 = args[1]
+	if len(data.axes) >= 5 :
+		msg.linear.z = data.axes[4] * scalers[2]	
 
-    end_effector = ur5.get_end_effector_link()
+	if len(data.axes) >= 4 :
+		msg.angular.y = data.axes[3] * scalers[4]
 
-    # Allow replanning to increase the odds of a solution
-    ur5.allow_replanning(True)
+	if len(data.axes) >= 3 :
+		msg.linear.y = data.axes[2] * scalers[1]
 
-    # Allow some leeway in position (meters) and orientation (radians)
-    ur5.set_goal_position_tolerance(0.01)
-    ur5.set_goal_orientation_tolerance(0.1)
+	if len(data.axes) >= 2 :	
+		msg.angular.z = data.axes[1] * scalers[5]
 
-    print('========= PLANNING ==========')
+	if len(data.axes) >= 1 :
+		msg.linear.x = data.axes[0] * scalers[0]
 
-    desired_pose = msg.position
-    print(desired_pose)
-    print('========================================================')
-
-    # ==================================== Cartesian Move Done
-    waypoint = []
-    # Not adding START POINT into WAYPOINT solved problem named
-    # "Trajectory message contains waypoints that are not strictly increasing in time."
-    start_point = ur5.get_current_pose(end_effector).pose
-
-    poseTarget = copy.deepcopy(start_point)
-
-    # 1st orient gripper and move forward/backward
-    poseTarget.position.x = desired_pose.x
-
-    # 2nd move sides
-    poseTarget.position.y = desired_pose.y
-
-    # 3rd move up/down
-    poseTarget.position.z = desired_pose.z
-
-    waypoint.append(copy.deepcopy(poseTarget))
-
-    # Set the internal state to the current state
-    ur5.set_start_state_to_current_state()
-
-    if np.sqrt((poseTarget.position.x - start_point.position.x)**2 +
-               (poseTarget.position.y - start_point.position.y)**2 +
-               (poseTarget.position.z - start_point.position.z)**2) < 0.1:
-        rospy.loginfo("Warnig: target position overlaps with the initial position!")
-
-    # Compute Cartesian path. The return value is a tuple: a fraction of how much of the path was followed, the actual
-    # RobotTrajectory.
-    (plan, fraction) = ur5.compute_cartesian_path(
-        waypoint,  # waypoints to follow
-        0.01,  # eef_step, which is set to 0.01m ~ 1 cm
-        0.0,  # jump_threshold, which is set to 0 to disable it. The jump_threshold specifies the maximum distance in
-        # configuration space between consecutive points in the resulting path
-        True  # collisions avoiding
-    )
-
-    if 1 - fraction < 0.2:
-        rospy.loginfo("Path computed successfully. Moving the arm.")
-        num_pts = len(plan.joint_trajectory.points)
-        rospy.loginfo("\n# intermediate waypoints = " + str(num_pts))
-
-        print('===== MOVING =====')
-        ur5.execute(plan, wait=True)
-        # ur5.go()
-        rospy.loginfo("Path execution complete.")
-        rospy.loginfo('followed ' + str(fraction*100) + '% of requested trajectory')  #fraction: rate of successful requested
-        # trajectory is followed
-    else:
-        rospy.loginfo("Path planning failed")
-
-    print('==== ROBOT STATE AFTER MOVING ====')
-    print(robot.get_current_state().joint_state.position)
-    print('=============')
-
-    print('==== EE POSE ====')
-    print(ur5.get_current_pose().pose)
-    print('=============')
-
-
-def subscriber():
-    print()
-    print('========================================================')
-
-    # Initializing moveit_commander and rospy
-    moveit_commander.roscpp_initialize(sys.argv)
-    rospy.init_node('ur5_cartesian_pose', anonymous=True)
-
-
-    # Instantiate a RobotCommander object.  This object is an interface to the robot as a whole.
-    robot = moveit_commander.RobotCommander()
-
-    # Instantiate a PlanningSceneInterface object.  This object is an interface to the world surrounding the robot.
-    scene = moveit_commander.PlanningSceneInterface()  # WHAT CAN WE DO WITH THIS???
-
-    # Instantiate UR5 as group of joints. This interface can be used to plan and execute motion of UR5
-    ur5 = moveit_commander.MoveGroupCommander('manipulator')
-
-    # Print some features of UR5
-    print('======= Reference frame:', ur5.get_planning_frame())
-    print('======= End Effector:', ur5.get_end_effector_link())
-    # Set the reference frame for pose targets
-    reference_frame = "/base_link"
-
-    # Set the ur5_arm reference frame accordingly
-    ur5.set_pose_reference_frame(reference_frame)
-
-    print('======= ROBOT STATE =======')
-    print(robot.get_current_state().joint_state.position)
-    print('================')
-
-    print('==== EE POSE ====')
-    print(ur5.get_current_pose().pose)
-    print('=============')
-
-    rospy.Subscriber('hololens', geometry_msgs.msg.Pose, move_ur5, (robot, ur5), queue_size = 1)
-
-    rospy.spin()
-
+	#### Publish msg
+	rate = rospy.Rate(100) # 10hz
+	rospy.loginfo(msg)
+	twist_publisher.publish(msg)
+	rate.sleep()
 
 if __name__ == '__main__':
-    try:
-        subscriber()
-
-    except rospy.ROSInterruptException:
-        # When finished shut down moveit_commander.
-        moveit_commander.roscpp_shutdown()
-        print('==== STOPPING ====')
-        pass
-
-
-# MOVING FROM [0.8 0.2 0.01] TO [-0.8 0.2 0.01] IS DANGEROUS
+	JoyToTwist()
